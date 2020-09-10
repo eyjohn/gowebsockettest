fcefd84c9d9470256eb768b659541cc225d4ce10
Evgeny Yakimov <john.yakimov@gmail.com>
2020-09-10 07:53:06 +0100

    Improved manual tracing
    - Don't use middleware tracer
    - Custom tracing of Message/Ping events
    - Propagate some errors to Spans

--- main.go
+++ main.go
@@ -11,19 +11,42 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
-	"github.com/opentracing-contrib/go-stdlib/nethttp"
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/config"
 )
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
+func populateCommonSpan(span opentracing.Span) {
+	span.SetTag("component", "main")
+	span.SetTag("span.kind", "server")
+
+}
+
+func populateHttpSpan(span opentracing.Span, req *http.Request) {
+	populateCommonSpan(span)
+	span.SetTag("http.method", req.Method)
+	span.SetTag("http.url", req.URL.String())
+	span.SetTag("peer.address", req.RemoteAddr)
+}
+
+func populateWebsocketSpan(span opentracing.Span, conn *websocket.Conn) {
+	populateCommonSpan(span)
+	span.SetTag("peer.address", conn.RemoteAddr())
+}
+
+func defaultHandler(w http.ResponseWriter, req *http.Request) {
+	span := opentracing.StartSpan("index")
+	populateHttpSpan(span, req)
+	defer span.Finish()
 	content, _ := ioutil.ReadFile("index.html")
 	w.Header().Set("Content-Type", "text/html")
 	w.Write(content)
 }
 
-func healthzHandler(w http.ResponseWriter, r *http.Request) {
+func healthzHandler(w http.ResponseWriter, req *http.Request) {
+	span := opentracing.StartSpan("healthz")
+	populateHttpSpan(span, req)
+	defer span.Finish()
 	w.Write([]byte("OK"))
 }
 
@@ -34,23 +57,34 @@ func websocketEcho(conn *websocket.Conn) {
 			log.Println("Read Error:", err)
 			break
 		}
+		span := opentracing.StartSpan("message")
+		populateWebsocketSpan(span, conn)
+		span.SetTag("message", string(message))
 		log.Printf("Received Message: %s from %v", message, conn.RemoteAddr())
 		err = conn.WriteMessage(mt, message)
 		if err != nil {
 			log.Println("Write Error:", err)
+			span.LogKV(
+				"event", "error",
+				"message", err)
+			span.Finish()
 			break
 		}
+		span.Finish()
 	}
 	conn.Close()
 }
 
 func websocketRandPing(conn *websocket.Conn) {
 	for {
+		span := opentracing.StartSpan("ping")
+		populateWebsocketSpan(span, conn)
 		err := conn.WriteMessage(websocket.TextMessage, []byte("randping"))
 		if err != nil {
 			log.Println(err)
 			return
 		}
+		span.Finish()
 		time.Sleep(time.Duration(rand.Intn(int(time.Second * 3))))
 	}
 }
@@ -61,10 +95,18 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-func websocketHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
+func websocketHandler(w http.ResponseWriter, req *http.Request) {
+	span := opentracing.StartSpan("websocket")
+	populateHttpSpan(span, req)
+	defer span.Finish()
+
+	conn, err := upgrader.Upgrade(w, req, nil)
 	if err != nil {
 		log.Println(err)
+		span.SetTag("error", true)
+		span.LogKV(
+			"event", "error",
+			"message", err)
 		return
 	}
 	log.Printf("Connection from %v", conn.RemoteAddr())
@@ -101,5 +143,5 @@ func main() {
 	http.HandleFunc("/healthz", healthzHandler)
 	http.HandleFunc("/websocket", websocketHandler)
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *portPtr), nethttp.Middleware(tracer, http.DefaultServeMux)))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *portPtr), nil))
 }
